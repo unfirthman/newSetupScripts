3c17d7d7f4bd0d7de802977e45c61ca6e7c427b0
unfirthman <unfirthman@gmail.com>
2023-12-03 12:06:48 -0500

    initial commit

---
+++ readMe.mk
@@ -0,0 +1,25 @@
+So you need to set up a new ubuntu dev environment.
+
+
+Here's what we need.
+
+
+NVIM
+
+
+
+
+Download and install new dot files
+
+
+
+
+Install packages:
+	NVM
+	Node.js
+	python
+	pip
+	pynvim
+
+
+Fonts
